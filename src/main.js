3dcb495b11f3c2a2ae6b58cba650e8297bca7978
Kacper Rozdolski <k.rpzdolski@gmail.com>
2021-04-16 12:50:14 +0200

    adding dist subtree

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-
-createApp(App)
-  .use(VueAxios, axios)
-  .mount("#app");
